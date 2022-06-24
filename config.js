252df71a7e02cde4a36ae17df224fff5c9184dce
mhetzel <michellehhetzel@gmail.com>
2022-06-24 09:30:35 -0500

    remove spinner

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
 
 async function loadConfig(event) {
   console.log('uploading config')
-  $("#spinner").show();
   const file = event.target.files.item(0);
   
   const text = await file.text();
-  $("#spinner").hide();
 
   PlantData = JSON.parse(text);
   await saveConfig(PlantData);
