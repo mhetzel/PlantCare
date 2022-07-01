45b19489c891f620b1bc6d18196fb8c2f15ef86c
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-07-01 10:33:25 -0500

    Update drive.js

--- drive.js
+++ drive.js
@@ -1,11 +1,6 @@
 const CLIENT_ID = '439504664142-ji2fdlinh9ueqc75mjvssemdouioe5o7.apps.googleusercontent.com';
 const API_KEY = 'AIzaSyCjsiD7ycx16Zv4WaREqnB5tuD9i447XAo';
-
-// Discovery doc URL for APIs used by the quickstart
 const DISCOVERY_DOC = 'https://www.googleapis.com/discovery/v1/apis/drive/v3/rest';
-
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
 const SCOPES = 'https://www.googleapis.com/auth/drive';
 
 let tokenClient;
@@ -93,25 +88,46 @@ function handleSignoutClick() {
   }
 }
 
-async function getFile(name) {
+async function getFileID(folderID) {
   let response;
   try {
     response = await gapi.client.drive.files.list({
-      'pageSize': 100,
+      'pageSize': 10,
       'fields': 'files(id, name)',
+      'q': "name = parents in '${folderID}' and name = 'data.json' and trashed != true"
     });
   } catch (err) {
-    return;
+    return null;
   }
   const files = response.result.files;
   if (!files || files.length == 0) {
-    return;
+    var fileContent = 'sample text updated'; // As a sample, upload a text file.
+    var file = new Blob([fileContent], {type: 'text/plain'});
+    var metadata = {
+        'name': 'data.json', // Filename at Google Drive
+        'mimeType': 'text/plain', // mimeType at Google Drive
+        'parents': [folderID], // Folder ID at Google Drive
+    };
+
+    var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
+    var form = new FormData();
+    form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
+    form.append('file', file);
+
+    fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id', {
+      method: 'POST',
+      headers: new Headers({ 'Authorization': 'Bearer ' + accessToken }),
+      body: form,
+    }).then((res) => {
+      return res.json();
+    }).then(function(val) {
+      console.log(val);
+    });
   }
 
   files.forEach( function(file) {
-     if (file.name == name) {
-       console.log(file.id)
-     }
+    console.log('Found: ', file.name, ': ', file.id)
+    return file.id
   });
 }
 
@@ -120,7 +136,7 @@ async function getFolderID() {
   let response;
   try {
     response = await gapi.client.drive.files.list({
-      'pageSize': 100,
+      'pageSize': 10,
       'fields': 'files(id, name)',
       'q': "mimeType = 'application/vnd.google-apps.folder' and name = 'PlantCare' and trashed != true"
     });
