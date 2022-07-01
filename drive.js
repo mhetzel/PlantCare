93a5f50e058c939bc36fb0006d9e092e8e2ded90
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-07-01 18:47:54 -0500

    Update drive.js

--- drive.js
+++ drive.js
@@ -10,6 +10,29 @@ let gisInited = false;
 document.getElementById('authorize_button').style.visibility = 'hidden';
 document.getElementById('signout_button').style.visibility = 'hidden';
 
+function parseJwt(token) {
+            var base64Url = token.split('.')[1];
+            var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+            var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+                return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+            }).join(''));
+
+            return JSON.parse(jsonPayload);
+        };
+      function onSignIn(googleUser) {
+        var token = googleUser.credential
+        var decoded = parseJwt(token);
+
+        console.log(decoded);
+      }
+      function signOut() {
+            console.log("bye");
+
+            google.accounts.id.disableAutoSelect();
+          
+        }
+
+
 /**
  * Callback after api.js is loaded.
  */
