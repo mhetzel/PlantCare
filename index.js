c94c55ce15011f171b4ebf4df1cbed6ef36c4de9
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-07-13 12:24:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,19 +3,9 @@ var PlantData = {}
 
 
 setupUserMode();
-setupDisplay();
-
 
 async function setupUserMode() {
   await initGoogleAPIs();
   determineUserMode();
   displayLoginPage(); 
 }
-
-
-function setupDisplay() {
-  dropdownSetup();
-  setDisplayForNoPlants();
-}
-
-
