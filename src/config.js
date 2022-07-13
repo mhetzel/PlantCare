861a8f7bf97612059db172419a7f5d41c44f45b1
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-07-13 08:46:00 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,69 @@
 const STORAGE_KEY = 'plantCareData'
+let DriveFileID = null;
+
+async function loadPlants() {
+  console.log('load plants')
+  if (typeof(Storage) !== "undefined") {
+    retrievePlantData()
+  } else {
+    alert('Sorry no way to store your plant info. Try a different browser')
+  }
+};
+
+async function saveConfig(plantData) {
+  await storePlantData(plantData)
+  // refresh displays
+  await dropdownSetup();
+  setDisplayForNoPlants();
+}
 
 
-async function loadConfig(event) {
+// TODO: check last updated timestamp to avoid losing data
+async function storePlantData(plantData) {
+  if (!GuestMode) {
+    await findOrCreateConfig();
+    writeFile(DriveFileID, plantData);
+  } else {
+    localStorage.setItem(StorageKey, JSON.stringify(plantData));
+  }
+};
+
+async function retrievePlantData() {  
+  if (!GuestMode) {
+    await findOrCreateConfig();
+    PlantData = readFile(DriveFileID)
+  } else {
+    var retrievedObject = localStorage.getItem(STORAGE_KEY);
+    if (retrievedObject === null) {
+      console.log('initializing plant data storage')
+      await saveConfig({});
+    } else {
+      PlantData = JSON.parse(retrievedObject);
+    }
+  }
+  console.log('loaded plant config: ', PlantData);
+};
+
+async function findOrCreateConfig() {
+  if (!GuestMode) {
+    if (FileID) {
+      getFolderID().then(folderID => { 
+        if (folderID) {
+          getFileID(folderID).then(fileID => {
+            DriveFileID = fileID;
+          });
+        } 
+      });
+    }
+  } else {
+    console.log('No drive access as Guest')
+  }
+};
+
+/*
+ * Local File Functions
+ */
+async function uploadConfig(event) {
   console.log('uploading config')
   const file = event.target.files.item(0);
   
@@ -17,27 +79,3 @@ function downloadConfig() {
   var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(PlantData));
   saveLink.attr("href", dataStr);
 };
-
-
-async function loadPlants() {
-  console.log('load plants')
-  if (typeof(Storage) !== "undefined") {
-    var retrievedObject = localStorage.getItem(STORAGE_KEY);
-    if (retrievedObject === null) {
-      await saveConfig(PlantData);
-      console.log('initializing plant data storage')
-    } else {
-      PlantData = JSON.parse(retrievedObject);
-      console.log('loaded plant config: ', PlantData);
-    }
-  } else {
-    alert('Sorry no way to store your plant info. Try a different browser')
-  }
-};
-
-async function saveConfig(plantData) {
-  localStorage.setItem(StorageKey, JSON.stringify(plantData));
-  await storePlantData(plantData)
-  await dropdownSetup();
-  setDisplayForNoPlants();
-}
