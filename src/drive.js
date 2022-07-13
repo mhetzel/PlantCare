fb7b43a2d6814a179b424c06e976f593b4ed320c
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-07-13 09:34:44 -0500

    Update drive.js

--- src/drive.js
+++ src/drive.js
@@ -35,43 +35,47 @@ async function writeFile(fileID, data) {
 async function getFileID(folderID) {
   let response;
   let id = null;
-  try {
-    response = await gapi.client.drive.files.list({
-      'pageSize': 10,
-      'fields': 'files(id, name)',
-      'q': "'" + folderID + "' in parents and name = 'data.json' and trashed != true"
-    });
-  } catch (err) {
-    console.log(err)
-  }
-  const files = response.result.files;
-  if (!files || files.length == 0) {
-    var fileContent = '';
-    var file = new Blob([fileContent], {type: 'text/plain'});
-    var metadata = {
-        'name': 'data.json', // Filename at Google Drive
-        'mimeType': 'text/plain', // mimeType at Google Drive
-        'parents': [folderID], // Folder ID at Google Drive
-    };
+  if (folderID) {
+    try {
+      response = await gapi.client.drive.files.list({
+        'pageSize': 10,
+        'fields': 'files(id, name)',
+        'q': "'" + folderID + "' in parents and name = 'data.json' and trashed != true"
+      });
+    } catch (err) {
+      console.log(err)
+      console.log('can\'t find file')
+    }
+    const files = response.result.files;
+    if (!files || files.length == 0) {
+      var fileContent = '';
+      var file = new Blob([fileContent], {type: 'text/plain'});
+      var metadata = {
+          'name': 'data.json', // Filename at Google Drive
+          'mimeType': 'text/plain', // mimeType at Google Drive
+          'parents': [folderID], // Folder ID at Google Drive
+      };
 
-    var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
-    var form = new FormData();
-    form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
-    form.append('file', file);
+      var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
+      var form = new FormData();
+      form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
+      form.append('file', file);
 
-    fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id', {
-      method: 'POST',
-      headers: new Headers({ 'Authorization': 'Bearer ' + accessToken }),
-      body: form,
-    }).then((response) => response.json()
-    ).then(function(file) {
-      id = file.id;
-      console.log('Created File ID: ', file.id);
-    }).catch(console.error);
-  } else {
-    console.log('Found: ', files[0].name, ': ', files[0].id);
-    id = files[0].id;
+      fetch('https://www.googleapis.com/upload/drive/v3/files?uploadType=multipart&fields=id', {
+        method: 'POST',
+        headers: new Headers({ 'Authorization': 'Bearer ' + accessToken }),
+        body: form,
+      }).then((response) => response.json()
+      ).then(function(file) {
+        id = file.id;
+        console.log('Created File ID: ', file.id);
+      }).catch(console.error);
+    } else {
+      console.log('Found: ', files[0].name, ': ', files[0].id);
+      id = files[0].id;
+    }
   }
+  
   return id;
 };
  
