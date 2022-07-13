aae292c605c7acf02cc0dfc2c60516a6ea9f0859
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-07-13 09:32:09 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -11,8 +11,9 @@ async function refreshDisplays() {
 async function loadPlants() {
   console.log('load plants')
   if (typeof(Storage) !== "undefined") {
-    retrievePlantData();
-    refreshDisplays();
+    retrievePlantData().then(
+      refreshDisplays();
+    );
   } else {
     alert('Sorry no way to store your plant info. Try a different browser')
   }
