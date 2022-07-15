5fcd69dcc73d331314576df7bcee9b9fbde99d99
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-07-15 10:16:50 -0500

    Update multiselect-dropdown.js

--- src/multiselect-dropdown.js
+++ src/multiselect-dropdown.js
@@ -107,7 +107,12 @@ function MultiselectDropdown(options){
           sels.map(x=>{
             var c=newEl('span',{class:'optext',text:x.text, srcOption: x});
             if((el.attributes['multiselect-hide-x']?.value !== 'true'))
-              c.appendChild(newEl('span',{class:'optdel',text:'🗙',title:config.txtRemove, onclick:(ev)=>{c.srcOption.listitemEl.dispatchEvent(new Event('click'));div.refresh();ev.stopPropagation();}}));
+              c.appendChild(newEl('span',{class:'optdel',text:'🗙',title:config.txtRemove, onclick:(ev)=>{
+                console.log('on click', ev, c)
+                c.srcOption.listitemEl.dispatchEvent(new Event('click'));
+                div.refresh();
+                ev.stopPropagation();
+              }}));
 
             div.appendChild(c);
           });
