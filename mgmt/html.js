9e8dbe07c948be353328552e1cf4b2a99d2772e5
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-07-25 12:27:22 -0500

    Update and rename loginHTML.js to html.js

--- mgmt/html.js
+++ mgmt/html.js
@@ -1,5 +1,6 @@
 //needs user.js
 //needs menu.js
+// needs config.js
 
 var loginDiv = $("#login-div");
 let signinDiv = $("signin-div");
@@ -9,18 +10,19 @@ let userNameText = $("user-name");
 let signOutButton = $("signout-button");
 
 
-function setupLoginDiv() {
-  let title = $(":contains(Plant Care Account Settings)");
-  let closeButtonDiv = createCloseButtonDiv();
-  closeButtonDiv.after(title);
+var configDiv = $("#config-div")
+
+function addCloseButtons() {
+  createCloseButtonDiv($(":contains(Config File Management)"));
+  createCloseButtonDiv($(":contains(Plant Care Account Settings)"));
 }
 
-function createCloseButtonDiv() {
+function createCloseButtonDiv(title) {
   let closeButtonDiv = $("<div></div>").addClass("top-right");
   let closeButton = $('<button title="Close"></button').click(closeAndShowPlants());
   let closeIcon = $('<i></i>').addClass("fa-solid fa-xmark");
   closeButton.append(closeIcon);
   closeButtonDiv.append(closeButton);
   
-  return closeButtonDiv;
+  closeButtonDiv.after(title);
 }
