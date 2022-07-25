607e3cddc2e26283ae68b127d9f289bf1bb3f547
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-07-25 12:25:30 -0500

    Update configHTML.js

--- mgmt/configHTML.js
+++ mgmt/configHTML.js
@@ -1,13 +1,3 @@
-// needs menu.js
-// needs config.js
-
-var configDiv = $("#config-div")
-
-function setupConfigDiv() {
-  let title = $(":contains(Config File Management)");
-  let closeButtonDiv = createCloseButtonDiv();
-  closeButtonDiv.after(title);
-}
 
 
 
