882432755d55376980280f4e472f45b71f170103
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-08-01 14:12:58 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ dropdown('#updated');
 initGoogleAPIs();
 
 
-$.getJSON('knownPlants.json', function(data) {
+$.getJSON('src/knownPlants.json', function(data) {
    $.each(data.knownPlants, function(i, f) {
        console.log(f)
        $("#known-plant").append($('<option></option>').attr('value', f).text(f));
