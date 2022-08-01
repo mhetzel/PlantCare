b899e63a17d19b303efe9268c8d3c04246726bed
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-08-01 14:09:06 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,15 @@
 setupNewPlantInput();
 dropdown('#updated');
 initGoogleAPIs();
+
+
+$.getJSON('knownPlants.json', function(data) {
+   $.each(data.knownPlants, function(i, f) {
+       console.log(f)
+       $("#known-plant").append($('<option></option>').attr('value', f).text(f));
+   });
+});
+
+
+
 $('select[multiple]').multiselect();
