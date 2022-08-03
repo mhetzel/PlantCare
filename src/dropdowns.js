434291abeae01d12c3f6da58891e96a885a0a656
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-08-03 13:24:16 -0500

    Update dropdowns.js

--- src/dropdowns.js
+++ src/dropdowns.js
@@ -31,15 +31,6 @@ function dropdown(prefix) {
   setDropdown($(prefix+'PlantHumitidy'), HumidityLevels);
 }
 
-function setupNewPlantInput() {
-  $("#newPlantName").val('');
-  $("#newPlantLocation").val('');
-  $("#newPlantLastWatered").val(null);
-  $("#newPlantAverageWateringDays").val(null);
-
-  dropdown('#new');
-}
-
 function setDropdown(dropdown, list) {
   dropdown.empty();
   list.forEach(function(x) {
