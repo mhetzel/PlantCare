777f3fd80ea46927a11f5fb66f6fb9141f8b6ed5
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-08-04 09:57:29 -0500

    Update plantHTML.js

--- src/plantHTML.js
+++ src/plantHTML.js
@@ -13,7 +13,7 @@ function displayPlant(element, locationName, plantName) {
   var averageDaysBetweenWatering = $('<span id="averageDaysBetweenWatering"></span>');
   
   var currentWetness = $('<select id="currentWetness"></select>');
-  currentWetness.change(checkPlant());
+  //currentWetness.change(checkPlant());
     
   var lastChecked = $('<span id="lastChecked"></span>');
   var lastWatered = $('<span id="lastWatered"></span>');
