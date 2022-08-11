926bc03d408db6b087f3d8dcbc916a4aa45cb08e
mhetzel <34289010+mhetzel@users.noreply.github.com>
2022-08-11 14:26:59 -0500

    Update plantActions.js

--- src/plantActions.js
+++ src/plantActions.js
@@ -19,9 +19,8 @@ function doesPlantNeedWatered(locationName, plantName) {
   let nextWateringDate = new Date(plant.nextWatering);
   
   let differenceCheckDate =  Math.floor((today - lastCheckedDate)/ (1000 * 3600 * 24))
-  let differenceWaterDate =  Math.floor((today - nextWateringDate)/ (1000 * 3600 * 24))
 
-  if (differenceCheckDate > 0 && differenceWaterDate > 0) {
+  if (differenceCheckDate > 0 && nextWateringDate <= today) {
     console.log(plantName, 'at', locationName, 'hasn\'t been checked today')
     console.log(plantName, 'at', locationName, 'probably needs watered because its past when the plant should have been watered')
     return true;
@@ -40,14 +39,13 @@ function doesPlantNeedChecked(locationName, plantName) {
   let nextWateringDate = new Date(plant.nextWatering);
   
   let differenceCheckDate =  Math.floor((today - lastCheckedDate)/ (1000 * 3600 * 24))
-  let differenceNextCheckDate =  Math.floor((today - nextCheckDate)/ (1000 * 3600 * 24))
   let differenceWaterDate =  Math.floor((today - nextWateringDate)/ (1000 * 3600 * 24))
   
-  if (differenceNextCheckDate > 0) {
+  if (nextCheckDate <= today) {
     console.log(plantName, 'at', locationName, 'needs checked because its halfwayish between last check and next watering')
     return true;
   }
-  else if (differenceCheckDate > 0 && differenceWaterDate > 0) {
+  else if (differenceCheckDate > 0 && nextWateringDate <= today) {
     console.log(plantName, 'at', locationName, 'hasn\'t been checked today')
     console.log(plantName, 'at', locationName, 'needs checked because its past when the plant should have been watered')
     return true;
