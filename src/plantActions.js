d3e9801a3434c1daa2f4abc0a0c113d90f6b8aeb
mhetzel <34289010+mhetzel@users.noreply.github.com>
2023-03-14 08:49:39 -0500

    Update plantActions.js

--- src/plantActions.js
+++ src/plantActions.js
@@ -172,38 +172,18 @@ $('#needsChecked').change(function() {
   showAllNeedyPlants(null);
 });
 
-$('#needsFertilized').change(function() {
-  showAllNeedyPlants(null);
-});
-
 
 function getNeedyPlants(locationName) {
   let needyPlants = []
-
-  if ($('#needsWater')[0].checked && $('#needsChecked')[0].checked && $('#needsFertilized')[0].checked) {
-    needyPlants = Object.keys(PlantData[locationName]).filter(plantName => doesPlantNeedFertilizer(locationName, plantName) || doesPlantNeedWatered(locationName, plantName) || doesPlantNeedChecked(locationName, plantName));
-  }
-  else if (!$('#needsWater')[0].checked && $('#needsChecked')[0].checked && $('#needsFertilized')[0].checked) {
-    needyPlants = Object.keys(PlantData[locationName]).filter(plantName => doesPlantNeedFertilizer(locationName, plantName) || doesPlantNeedChecked(locationName, plantName));
-  }
-  else if ($('#needsWater')[0].checked && !$('#needsChecked')[0].checked && $('#needsFertilized')[0].checked) {
-    needyPlants = Object.keys(PlantData[locationName]).filter(plantName => doesPlantNeedFertilizer(locationName, plantName) || doesPlantNeedWatered(locationName, plantName));
-  }
-  else if ($('#needsWater')[0].checked && $('#needsChecked')[0].checked && !$('#needsFertilized')[0].checked) {
+  if ($('#needsWater')[0].checked && $('#needsChecked')[0].checked) {
     needyPlants = Object.keys(PlantData[locationName]).filter(plantName => doesPlantNeedWatered(locationName, plantName) || doesPlantNeedChecked(locationName, plantName));
-  }
-  else if (!$('#needsWater')[0].checked && !$('#needsChecked')[0].checked && $('#needsFertilized')[0].checked) {
-    needyPlants = Object.keys(PlantData[locationName]).filter(plantName => doesPlantNeedFertilizer(locationName, plantName));
-  }
-  else if ($('#needsWater')[0].checked && !$('#needsChecked')[0].checked && !$('#needsFertilized')[0].checked) {
+  } else if ($('#needsWater')[0].checked && !$('#needsChecked')[0].checked) {
     needyPlants = Object.keys(PlantData[locationName]).filter(plantName => doesPlantNeedWatered(locationName, plantName));
-  }
-  else if (!$('#needsWater')[0].checked && $('#needsChecked')[0].checked && !$('#needsFertilized')[0].checked) {
+  } else if (!$('#needsWater')[0].checked && $('#needsChecked')[0].checked) {
     needyPlants = Object.keys(PlantData[locationName]).filter(plantName => doesPlantNeedChecked(locationName, plantName));
   }
-
   if (needyPlants.length > 0) {
-    needyPlants.sort(function compareFn(a, b) { return comparePlantNeeds(locationName, a, b) })
+  	needyPlants.sort(function compareFn(a, b) { return comparePlantNeeds(locationName, a, b) })
   }
   return needyPlants
 }
