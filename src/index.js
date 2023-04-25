b06ea36e23a86009c6a181be963a0cadf632704a
mhetzel <34289010+mhetzel@users.noreply.github.com>
2023-04-25 14:11:32 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,29 +2,5 @@ addCloseButtons();
 setCurrentUserDisplay('Guest', null);
 setupNewPlantInput();
 setKnownPlantDropdown();
-//initGoogleAPIs();
-
-
-var eventHandler = function(name) {
-  return function() {
-    console.log(name, arguments);
-  };
-};
-var $select = $('#select-state').selectize({
-  create          : true,
-  maxItems: 1,
-  onChange        : eventHandler('onChange'),
-  onItemAdd       : eventHandler('onItemAdd'),
-  onItemRemove    : eventHandler('onItemRemove'),
-  onOptionAdd     : eventHandler('onOptionAdd'),
-  onOptionRemove  : eventHandler('onOptionRemove'),
-  onDropdownOpen  : eventHandler('onDropdownOpen'),
-  onDropdownClose : eventHandler('onDropdownClose'),
-  onFocus         : eventHandler('onFocus'),
-  onBlur          : eventHandler('onBlur'),
-  onInitialize    : eventHandler('onInitialize'),
-});
-
-
 
 $('select[multiple]').multiselect();
