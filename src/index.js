94c02975a3ab9ad4ef7816741eb88ec9895a2e4e
mhetzel <34289010+mhetzel@users.noreply.github.com>
2023-04-25 13:09:38 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,17 +5,6 @@ setKnownPlantDropdown();
 //initGoogleAPIs();
 
 
-$('#select-tools').selectize({
-  maxItems: null,
-  valueField: 'id',
-  labelField: 'title',
-  searchField: 'title',
-  options: [
-    {id: 1, title: 'Spectrometer', url: 'http://en.wikipedia.org/wiki/Spectrometers'},
-    {id: 2, title: 'Star Chart', url: 'http://en.wikipedia.org/wiki/Star_chart'},
-    {id: 3, title: 'Electrical Tape', url: 'http://en.wikipedia.org/wiki/Electrical_tape'}
-  ],
-  create: true
-});
+$('#normalize').selectize();
 
 $('select[multiple]').multiselect();
