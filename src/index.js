4e56a62cb39951a02a5399971a27a22537cfa3a4
mhetzel <34289010+mhetzel@users.noreply.github.com>
2023-04-25 13:21:33 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,25 @@ setKnownPlantDropdown();
 //initGoogleAPIs();
 
 
-$('#normalize').selectize();
+var eventHandler = function(name) {
+  return function() {
+    console.log(name, arguments);
+  };
+};
+var $select = $('#normalize').selectize({
+  create          : true,
+  onChange        : eventHandler('onChange'),
+  onItemAdd       : eventHandler('onItemAdd'),
+  onItemRemove    : eventHandler('onItemRemove'),
+  onOptionAdd     : eventHandler('onOptionAdd'),
+  onOptionRemove  : eventHandler('onOptionRemove'),
+  onDropdownOpen  : eventHandler('onDropdownOpen'),
+  onDropdownClose : eventHandler('onDropdownClose'),
+  onFocus         : eventHandler('onFocus'),
+  onBlur          : eventHandler('onBlur'),
+  onInitialize    : eventHandler('onInitialize'),
+});
+
+
 
 $('select[multiple]').multiselect();
