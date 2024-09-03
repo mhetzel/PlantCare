2ced59db1dc20df58d1f56d2ad713d07f54291af
mhetzel <34289010+mhetzel@users.noreply.github.com>
2024-09-03 16:11:44 -0500

    Update drive.js

--- src/google/drive.js
+++ src/google/drive.js
@@ -45,30 +45,20 @@ async function readFile(fileID) {
  
 async function writeFile(fileID, data, fileName) {
   await checkAccess(); 
-  console.log('Attempting to write to', fileID)
-
-  var file = new Blob([data], {type: 'text/plain'});
-  var metadata = {
-      'name': fileName, // Filename at Google Drive
-      'mimeType': 'text/plain', // mimeType at Google Drive
-      'parents': ['appDataFolder'], // Folder ID at Google Drive
-  }
-
-  var accessToken = gapi.auth.getToken().access_token; // Here gapi is used for retrieving the access token.
-  var form = new FormData();
-  form.append('metadata', new Blob([JSON.stringify(metadata)], { type: 'application/json' }));
-  form.append('file', file);
   const url = 'https://www.googleapis.com/upload/drive/v3/files/' + fileID + '?uploadType=media';
   fetch(url, {
     method: 'PATCH',
-    headers: new Headers({ 'Authorization': 'Bearer ' + accessToken }),
-    body: form,
-  }).then((response) => response.json()
-  ).then(function(file) {
-    id = file.id;
-    console.log('Updated File ID: ', file.id);
-  }).catch(console.error);
-
+    headers: new Headers({
+        Authorization: 'Bearer ' + gapi.auth.getToken().access_token,
+        'Content-type': 'text/plain'
+    }),
+    body: JSON.stringify(data)
+  })
+  .then(result => result.json())
+  .then(value => {
+    console.log('Updated. Result:\n' + JSON.stringify(value, null, 2));
+  })
+  .catch(err => console.error(err));
 }
  
 async function getFileID(fileName) {
