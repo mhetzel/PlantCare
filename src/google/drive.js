7da6902e821027a6f0c0b8afc907f095333f206b
mhetzel <34289010+mhetzel@users.noreply.github.com>
2024-09-09 15:11:14 -0500

    Update src/google/drive.js

--- src/google/drive.js
+++ src/google/drive.js
@@ -13,11 +13,12 @@ async function checkAccess() {
     return true;
   }, function(reason) {
     console.log('check access', reason.result.error.status)
-//     if (reason.result.error.message === 'Invalid Credentials' || reason.result.error.message === 'The user does not have sufficient permissions for this file.') {
-// //       tokenClient.requestAccessToken();
-// //       google.accounts.id.prompt();
-//       return false;
-//     }
+     if (reason.result.error.message === 'Invalid Credentials' || reason.result.error.message === 'The user does not have sufficient permissions for this file.') {
+       tokenClient.requestAccessToken();
+       google.accounts.id.prompt();
+       return false;
+     }
+
     if (reason.result.error.status === "UNAUTHENTICATED") {
       tokenClient.requestAccessToken();
       google.accounts.id.prompt();
